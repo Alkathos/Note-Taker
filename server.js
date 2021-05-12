7602e5fd95ab8a83a9cde1200b1e80dd77478ed9
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-05-12 09:49:46 -0400

    Added functionality to navigate to the webpages after running server

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const fs = require("fs");
 
 const server = new express();
 
+server.get("/", (req, res) => {
+    fs.readFile(`${__dirname}/public/index.html`, "utf-8", (err, html) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(html);
+        }
+    })
+})
 
+server.get("/notes", (req, res) => {
+    fs.readFile(`${__dirname}/public/notes.html`, "utf-8", (err, html) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(html);
+        }
+    })
+})
 
 server.listen(3000, () => {
     console.log("Server is up and running on port 3000.")
